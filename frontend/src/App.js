9d26cb63ae4c9ef6a7aa3df16b61f6d33298fbcf
Leon Michalak <leonmichalak6@gmail.com>
2022-06-30 15:10:37 +0100

    Added colour/typography CSS variables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,7 @@
 import './base.css';
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <p>Starter</p>;
 }
 
 export default App;
