2eff60785c16a9aa7807ae266e591667a5d0e720
Leon Michalak <leonmichalak6@gmail.com>
2022-06-30 16:05:11 +0100

    All routes added so sidebar is working, just rendering homepage at the moment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.js';
+import Movies from './pages/Home/Home.js';
+import TVSeries from './pages/Home/Home.js';
+import Bookmarks from './pages/Home/Home.js';
 import Sidebar from './components/Sidebar/Sidebar.js';
 import './pageLayout.css';
 import './base.css';
@@ -12,6 +15,9 @@ function App() {
         <Sidebar />
         <Routes>
           <Route exact path='/' element={<Home />} />
+          <Route exact path='/movies' element={<Movies />} />
+          <Route exact path='/tv-series' element={<TVSeries />} />
+          <Route exact path='/bookmarks' element={<Bookmarks />} />
         </Routes>
       </div>
     </Router>
